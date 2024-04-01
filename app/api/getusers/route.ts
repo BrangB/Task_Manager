48104ed4e2a7dc15ef18778f4359ff5bfe0c3134
Brag <brangtsawmaung89@gmail.com>
2024-04-01 23:24:20 +0630

    add get users route

--- app/api/getusers/route.ts
+++ app/api/getusers/route.ts
@@ -1,11 +1,11 @@
 import {NextResponse } from "next/server";
 import prisma from "@/app/utils/connect";
 
-export async function GET(req: Request) {
+export async function GET() {
     try {
         const users = await prisma.user_data.findMany();
 
-        return NextResponse.json({ users });
+        return NextResponse.json(users);
     } catch (error) {
         console.error("Error fetching users:", error);
         return NextResponse.json({ error: "Fetching Users Error", status: 500 });
