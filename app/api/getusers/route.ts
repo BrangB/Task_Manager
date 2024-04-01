d67985599a6a28da6591dd8d26d86e8709cf7092
Brag <brangtsawmaung89@gmail.com>
2024-04-01 23:42:16 +0630

    did some c

--- app/api/getusers/route.ts
+++ app/api/getusers/route.ts
@@ -1,10 +1,19 @@
 import {NextResponse } from "next/server";
 import prisma from "@/app/utils/connect";
 
-export async function GET() {
-    try {
-        const users = await prisma.user_data.findMany();
+// export async function GET() {
+//     try {
+//         const users = await prisma.user_data.findMany();
+//         return NextResponse.json(users);
+//     } catch (error) {
+//         console.error("Error fetching users:", error);
+//         return NextResponse.json({ error: "Fetching Users Error", status: 500 });
+//     }
+// }
 
+export const GET = async() => {
+    try {
+        const users = await prisma?.user_data.findMany();
         return NextResponse.json(users);
     } catch (error) {
         console.error("Error fetching users:", error);
